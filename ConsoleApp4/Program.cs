ec9582f23bd77a27595a49cc6c67269ac411e4a8
w10 <w10@DESKTOP-I7GQVM8>
2021-10-01 22:16:37 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Vehiculo
+    {
+        private string color;
+        private double motor;
+
+        public double color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+
+        public string Motor
+        {
+            get
+            {
+                return Motor;
+            }
+            set
+            {
+                Vehiculo = value;
+            }
+        }
+        public Vehiculo()
+        {
+            color = "rojo";
+            Motor = 2.3;
+        }
+        public Vehiculo(string color, double motor)
+        {
+            this.color = color;
+            this.Motor = Motor;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "--> color:" + "motor" + motor;
+        }
+
+
+
+
+
